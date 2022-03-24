407216c9226dc2d933ab8fead8b3fc4aea2ceb66
roy-zz <roystreet91@gmail.com>
2022-03-24 09:16:20 +0900

    add soccer player data repository
    add team data repository

--- src/main/java/com/roy/datajpa/repository/data/TeamDataRepository.java
+++ src/main/java/com/roy/datajpa/repository/data/TeamDataRepository.java
@@ -1,9 +1,9 @@
 package com.roy.datajpa.repository.data;
 
-import com.roy.datajpa.domain.SoccerPlayer;
+import com.roy.datajpa.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SoccerPlayerDataRepository extends JpaRepository<SoccerPlayer, Long> {
+public interface TeamDataRepository extends JpaRepository<Team, Long> {
 }
